e43d4947246dc5f2c1b31a6838c2ba0728c7ea3d
golbeck <smgolbeck@gmail.com>
2014-06-27 17:14:04 -0700

    second

--- test_ipython.py
+++ test_ipython.py
@@ -1,13 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from pandas import Series, DataFrame
+import pandas as pd
 
 def main():
-    points = np.arange(-5,5,0.1)
-    xs,ys=np.meshgrid(points,points)
-    print xs
-    z=np.sqrt(xs**2+ys**2)
-    print z
-    plt.imshow(z,cmap=plt.cm.gray);plt.colorbar()
 
 if __name__ == '__main__':
     main()
