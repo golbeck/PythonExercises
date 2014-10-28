c2fdb0b800629d3d80ab5576db5f3f6797af15c4
golbeck <smgolbeck@gmail.com>
2014-10-28 16:56:22 -0700

    stocktwits_API

--- twitter/stocktwits.py
+++ twitter/stocktwits.py
@@ -1,60 +1,35 @@
 ###########################################################################################################
 ###########################################################################################################
-#generate token (it is passed in the resulting url)
-https://api.stocktwits.com/api/2/oauth/authorize?client_id=e6ea615fc3943c04&response_type=token&redirect_uri=http://www.stocktwits.com&scope=read,watch_lists,publish_messages,publish_watch_lists,follow_users,follow_stocks
+#generate token by loading below URL in browser (it is passed in the resulting url)
+#https://api.stocktwits.com/api/2/oauth/authorize?client_id=e6ea615fc3943c04&response_type=token&redirect_uri=http://www.stocktwits.com&scope=read,watch_lists,publish_messages,publish_watch_lists,follow_users,follow_stocks
 ###########################################################################################################
 ###########################################################################################################
-#see https://developer.linkedin.com/documents/getting-oauth-token-python
-#see https://github.com/simplegeo/python-oauth2
-#see http://stocktwits.com/developers/docs/authentication#responses
-#see http://stocktwits.com/developers/docs/api#oauth-token-docs
 #tools for accessing the API
 import oauth2 as oauth
-<<<<<<< HEAD
-import urllib2 as urllib
 #tweets are output in JSON format. Use this package to load the data into python
 import json
-=======
-import urlparse 
+#save output to dataframe and then to csv
+import pandas as pd
 
-curl -X POST https://api.stocktwits.com/api/2/oauth/token -d 'client_id=e6ea615fc3943c04&client_secret=cd49fc9a46fa1938c11ca8b45d157aabd7c6e067&code=<code>&grant_type=authorization_code&redirect_uri=http://www.example.com'
+###########################################################################################################
+###########################################################################################################
+#use for processing the tweets
+import nltk
+from nltk import word_tokenize
 
-curl -X GET https://api.stocktwits.com/api/2/oauth/authorize -d 'client_id=e6ea615fc3943c04&response_type=code&redirect_uri=http://www.stocktwits.com&scope=read,watch_lists,publish_messages,publish_watch_lists,follow_users,follow_stocks'
->>>>>>> 108031d34579cf5065218ff827933907f4c28446
-
-consumer_key = "e6ea615fc3943c04"
-consumer_secret = "cd49fc9a46fa1938c11ca8b45d157aabd7c6e067"
-access_token='7d12b1c80b938756360078a3cfbd1651acd8b302'
-# Create your consumer with the proper key/secret.
-<<<<<<< HEAD
-oauth_consumer = oauth.Consumer(key=consumer_key,secret=consumer_secret)
-
-=======
-consumer = oauth.Consumer(key=consumer_key,secret=consumer_secret)
-
-# Request token URL for Twitter.
-#request_token_url = "https://api.stocktwits.com/api/2/oauth/token"
-request_token_url = "https://api.stocktwits.com/api/2/oauth/authorize"
->>>>>>> 108031d34579cf5065218ff827933907f4c28446
-
-# Create our client.
-client = oauth.Client(oauth_consumer)
-url='https://api.stocktwits.com/api/2/streams/suggested.json?access_token='
-#download the maximum number of tweets
-url=url+access_token
-# The OAuth Client request works just like httplib2 for the most part.
-resp, content = client.request(url, "GET")
-print resp
-print content
-if resp['status'] != '200':
-    raise Exception("Invalid response %s." % resp['status'])
-
-response=json.loads(content)
-keys_=response.keys()
-dict_cursor=response['cursor']
-max_id=dict_cursor['max']-1
-min_id=dict_cursor['since']
-messages=response['messages']
+#regular expressions package
+import re
+#pattern for what the tokens are when using nltk.regexp_tokenize(text,pattern)
+#for more info see: http://www.nltk.org/book/ch03.html
+pattern = r'''(?x)    # set flag to allow verbose regexps
+    ([A-Z]\.)+        # abbreviations, e.g. U.S.A.
+    | \w+(-\w+)*        # words with optional internal hyphens
+    | \$?\d+(\.\d+)?%?  # currency and percentages, e.g. $12.40, 82%
+    | \$\w+  # currency and percentages, e.g. $12.40, 82%
+    | @\S+             # tokenize twitter mentions
+    | \.\.\.            # ellipsis
+    | [][.,;"'?():-_`]  # these are separate tokens; includes ], [
+    '''
 ###########################################################################################################
 ###########################################################################################################
 def stocktwits_request(max_id):
@@ -71,8 +46,9 @@ def stocktwits_request(max_id):
     url='https://api.stocktwits.com/api/2/streams/suggested.json?access_token='
     #download the maximum number of tweets
     url=url+access_token
+    #query by max_id
     url=url+'&max='+max_id
-    # The OAuth Client request works just like httplib2 for the most part.
+    # OAuth Client request
     try:
         resp, content = client.request(url, "GET")
         response=json.loads(content)
@@ -82,21 +58,88 @@ def stocktwits_request(max_id):
         resp={}
         resp['x-ratelimit-remaining']=0
     return {'content':response,'limit':resp['x-ratelimit-remaining']}
+###########################################################################################################
+###########################################################################################################
+def text_processing(text):
+    temp_text=text.strip()
+    temp_text=re.sub('[,;"\'?():_`/\.]','',temp_text)
 
+    #custom tokenizer using the pattern defined above (beginning of file)
+    temp_text=nltk.regexp_tokenize(temp_text, pattern)
+    #covnert from unicode to string
+    temp_text=[w.encode('utf-8') for w in temp_text]
 
+    #find hashtags and remove #
+    temp_text=[re.sub(r'#','',w) for w in temp_text]
+
+    #get rid of links
+    temp_text=[w for w in temp_text if not w.startswith('htt')]
+    return temp_text
+###########################################################################################################
+###########################################################################################################
 limit=400
-M=400
-message=[]
+M=0
+messages=[]
 max_id=''
 while limit>M:
     dict1=stocktwits_request(max_id)
     response=dict1['content']
     max_id=response['cursor']['max']-1
     max_id=str(max_id)
+    since_id=response['cursor']['since']
     message=response['messages']
     [messages.append(x) for x in message]
     limit=int(dict1['limit'])
-    print limit
+    print limit, max_id, since_id, len(messages)
+###########################################################################################################
+###########################################################################################################
+N=len(messages)
+text=[text_processing(messages[i]['body']) for i in range(N)]
+created_at=[messages[i]['created_at'] for i in range(N)]
+message_id=[messages[i]['id'] for i in range(N)]
+symbol=[messages[i]['symbols'][0]['symbol'] for i in range(N)]
+classification=[messages[i]['user']['classification'] for i in range(N)]
+followers=[messages[i]['user']['followers'] for i in range(N)]
+id_=[messages[i]['user']['id'] for i in range(N)]
+trading_strategy=[messages[i]['user']['trading_strategy']['approach'] for i in range(N)]
+assets_frequently_traded=[messages[i]['user']['trading_strategy']['assets_frequently_traded'] for i in range(N)]
+experience=[messages[i]['user']['trading_strategy']['experience'] for i in range(N)]
+holding_period=[messages[i]['user']['trading_strategy']['holding_period'] for i in range(N)]
+username=[messages[i]['user']['username'] for i in range(N)]
 
 
+#generate date, time of day, day of week
+day_week=[messages[i]['created_at'].encode('utf8')[10] for i in range(N)]
+date_=[messages[i]['created_at'].encode('utf8')[0:10] for i in range(N)]
+time_=[messages[i]['created_at'].encode('utf8')[11:19] for i in range(N)]
+tz_=[messages[i]['created_at'].encode('utf8')[19] for i in range(N)]
+
+
+###########################################################################################################
+###########################################################################################################
+DF_stocktwits=pd.DataFrame()
+DF_stocktwits['text']=text
+DF_stocktwits['day']=day_week
+DF_stocktwits['date']=date_
+DF_stocktwits['time']=time_
+DF_stocktwits['tz']=tz_
+DF_stocktwits['message_id']=message_id
+DF_stocktwits['symbol']=symbol
+DF_stocktwits['classification']=classification
+DF_stocktwits['followers']=followers
+DF_stocktwits['id']=id_
+DF_stocktwits['strategy']=trading_strategy
+DF_stocktwits['assets_traded']=assets_frequently_traded
+DF_stocktwits['experience']=experience
+DF_stocktwits['holding_per']=holding_period
+DF_stocktwits['username']=username
+
+###########################################################################################################
+###########################################################################################################
+current_time=datetime.datetime.now()
+hr=str(current_time.hour)
+mn=str(current_time.minute)
+current_time=str(hr+'_'+mn)
+str_csv='Stocktwits/'+date_[-1]+'_'+current_time+'PST'+'.csv'
+DF_stocktwits.to_csv(str_csv,sep=',',header=True,index=True)
 
